e5361e5b8de5bb80d8d871aa7e0ecb7176f7f649
NoritakaTakehara <50361226+NoritakaTakehara@users.noreply.github.com>
2020-04-18 20:06:42 +0900

    フォロー機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit 
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_followers'
+  end
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:nickname,:email,:avatar)
+  end
+end

96992c79f4a6005af9e0fbb6f4e1f6dacc332a52
NoritakaTakehara <50361226+NoritakaTakehara@users.noreply.github.com>
2020-04-19 13:54:11 +0900

    deploy修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,8 @@ set :rbenv_ruby, '2.4.9' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/Desktop/favorite-ssh-key.pem'] 
+                  keys: ['~/Desktop/favorite-ssh-key.pem'],
+                  forward_agent: true
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
